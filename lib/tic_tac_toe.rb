92a65364b19d9c05d320be20b7f43c4344c03df8
Dina Kendle <dina.kendle@gmail.com>
2020-04-19 14:50:41 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,22 @@
 class TicTacToe
   attr_accessor :board
-  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+    [8,5,2]
+    ]
   
   def initialize
     @board = board || Array.new(9, " ")
   end
   
+  def display_board
+    
+  end
   
 end
