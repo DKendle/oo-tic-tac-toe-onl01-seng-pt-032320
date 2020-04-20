e21abc935accfef600d88e72dcb0785ee7d424b0
Dina Kendle <dina.kendle@gmail.com>
2020-04-20 02:33:04 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ class TicTacToe
   end
   
   def input_to_index(input)
-    input.to_i - 1 
+    input.to_i - 1
   end 
   
   def move(index, token = "X")
@@ -43,22 +43,67 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn 
+  def turn_count
+    @board.count {|token| token == "X" || token == "O"}
   end
   
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
   
+  def turn
+    puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+      cp = current_player
+      index = input_to_index(user_input)
+        if valid_move?(index)
+          move(index, current_player)
+          display_board
+        else
+          turn
+        end
+  end
+     
+     def won?
+      a = WIN_COMBINATIONS.find{|combo|
+      @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+    }
+    
+    b = WIN_COMBINATIONS.find{|combo|
+      @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+    }
+    return a || b
+  end
   
+  def full?
+    !@board.any?{|moves| moves =="" || moves ==" "}
+  end
   
+  def draw?
+    !won? && full?
+  end 
   
+  def over?
+    won? || draw?
+  end
   
+  def winner
+    if won?
+      @board[won?[0]] == "X" ? "X" : "O"
+    else
+      nil
+    end
+  end
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  def play 
+    @board = board || Array.new(9, " ")
+    turn until over?
+     won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+     
+    
+  end
+game = TicTacToe.new
+game.play
 end
+
